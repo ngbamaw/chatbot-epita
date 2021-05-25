3e86b0ffc03dc5b640122b94a10a79761859d27f
PellYm785 <williamliya@hotmail.fr>
2021-05-25 15:50:34 +0200

    feat: initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,124 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import SimpleBar from 'simplebar-react';
+import SendIcon from './assets/send-icon.png';
+import PlusIcon from './assets/plus.png';
+import Style from './style';
+import 'simplebar/dist/simplebar.min.css';
+
+interface Message {
+    author?: string;
+    text: string;
+}
+
+const ReceiverMessage: React.FC<{ children: string }> = ({ children }) => (
+    <div className="message receiver">
+        <p>{children}</p>
+    </div>
+);
+
+const MeMessage: React.FC<{ children: string }> = ({ children }) => (
+    <div className="message me">
+        <p>{children}</p>
+    </div>
+);
+
+const Bot: React.FC<{ name: string; description: string }> = ({ name, description }) => (
+    <div className="bot">
+        <div className="icon" />
+        <div className="description">
+            <div className="name">{name}</div>
+            <p className="text">Description of the bot, what it can do, how it’s work, etc...</p>
+        </div>
+    </div>
+);
 
 const App: React.FC = () => {
+    const [messageList, setMessageList] = React.useState<Message[]>([
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+    ]);
+    const [responseWriting, setResponseWriting] = React.useState<boolean>(false);
+    const [command, setCommand] = React.useState<string>('');
+    const discussionSection = React.useRef<HTMLDivElement>(null);
+    const container = React.useRef<HTMLDivElement>(null);
+    const scrollbarWrapper = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        if (scrollbarWrapper.current) scrollbarWrapper.current.scrollTop = 5000;
+    }, [messageList]);
+
+    React.useEffect(() => {
+        if (discussionSection.current && scrollbarWrapper.current)
+            discussionSection.current.style.height = `${scrollbarWrapper.current.offsetHeight}px`;
+    }, [discussionSection]);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+        <Style ref={container}>
+            <div className="header-app">
+                <div className="icon" />
+            </div>
+            <div className="bots-list">
+                <div className="header-bot-list">
+                    <button className="round-btn add-btn" type="button">
+                        <img src={PlusIcon} alt="add" />
+                    </button>
+
+                    <div className="round-textfield search-bot">
+                        <input type="text" placeholder="Search bot" />
+                    </div>
+                </div>
+                <SimpleBar className="scrollbar">
+                    <Bot
+                        name="Bot's name"
+                        description="Description of the bot, what it can do, how it’s work, etc..."
+                    />
+                </SimpleBar>
+            </div>
+            <div className="main-content">
+                <SimpleBar
+                    className="scrollbar-discussion"
+                    scrollableNodeProps={{ ref: scrollbarWrapper }}
                 >
-                    Learn React
-                </a>
-            </header>
-        </div>
+                    <div className="discussion-section" ref={discussionSection}>
+                        {messageList.map(({ author, text }) => {
+                            return author === 'me' ? (
+                                <MeMessage>{text}</MeMessage>
+                            ) : (
+                                <ReceiverMessage>{text}</ReceiverMessage>
+                            );
+                        })}
+                        {responseWriting && (
+                            <div className="pending-info">
+                                <div className="circle" />
+                                <div className="circle" />
+                                <div className="circle" />
+                            </div>
+                        )}
+                    </div>
+                </SimpleBar>
+                <div className="bottom">
+                    <div className="round-textfield command-writer">
+                        <input
+                            type="text"
+                            placeholder="Write command"
+                            onChange={(e) => setCommand(e.target.value)}
+                            value={command}
+                        />
+                    </div>
+
+                    <button
+                        className="round-btn send-btn"
+                        type="button"
+                        onClick={() => {
+                            setMessageList([...messageList, { author: 'me', text: command }]);
+                            setCommand('');
+                        }}
+                    >
+                        <img src={SendIcon} alt="send" />
+                    </button>
+                </div>
+            </div>
+        </Style>
     );
 };
 
---
+++ src/style/index.js
@@ -0,0 +1,216 @@
+import styled, { css } from 'styled-components';
+
+function createCSS() {
+    let styles = '';
+
+    for (let i = 1; i <= 3; i++) {
+        styles += `
+         .circle:nth-of-type(${i}) {
+           animation: animate-circle 500ms infinite alternate;
+           animation-delay: ${i * 0.2}s;
+         }
+       `;
+    }
+
+    return css`
+        ${styles}
+    `;
+}
+
+const App = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-wrap: wrap;
+    font-family: 'Roboto', sans-serif;
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    .header-app {
+        width: 100%;
+        height: 10%;
+        box-shadow: rgba(0, 0, 0, 0.25) 0 5px 20px;
+        margin-bottom: 24px;
+    }
+    .round-textfield {
+        background-color: #e9e9e9;
+        height: 100%;
+        border: none;
+        border-radius: 50px;
+        display: flex;
+        input {
+            margin: auto 24px;
+            width: 100%;
+            background-color: #e9e9e9;
+            border: none;
+            padding: 0;
+            font-size: 18px;
+            &:focus {
+                outline: none;
+                border: none;
+            }
+            &::placeholder {
+                font-weight: bold;
+                font-size: large;
+            }
+        }
+    }
+    .round-btn {
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        border: none;
+        background-color: #2196f3;
+        display: flex;
+        align-self: flex-end;
+        margin-left: auto;
+        margin-right: 15px;
+        outline: none;
+        img {
+            height: 55%;
+            width: 55%;
+            margin: auto;
+        }
+    }
+    .bots-list {
+        width: 20%;
+        height: calc(90% - 24px);
+        display: flex;
+        flex-direction: column;
+        .header-bot-list {
+            display: flex;
+            height: 50px;
+            margin-left: 24px;
+            margin-bottom: 24px;
+            .search-bot {
+                flex: 1;
+                height: 50px;
+            }
+            .add-btn {
+                background-color: #e9e9e9;
+                margin-right: 24px;
+            }
+        }
+        .bot {
+            display: flex;
+            margin-left: 24px;
+            margin-bottom: 24px;
+            .icon {
+                height: 100px;
+                width: 100px;
+                border-radius: 50%;
+                background-color: #e9e9e9;
+                margin-right: 18px;
+            }
+            .description {
+                width: 60%;
+                overflow: hidden;
+                margin: auto 0;
+                .name {
+                    margin-bottom: 12px;
+                    font-weight: bold;
+                    font-size: x-large;
+                }
+            }
+        }
+
+        .scrollbar {
+            flex: 1;
+            height: 100px;
+            overflow: auto;
+        }
+    }
+    .main-content {
+        width: 80%;
+        height: calc(90% - 24px);
+        display: flex;
+        flex-direction: column;
+        .discussion-section {
+            display: flex;
+            justify-content: flex-end;
+            flex-direction: column;
+            position: relative;
+            width: 100%;
+        }
+        .scrollbar-discussion {
+            flex: 1;
+            height: 100px;
+            overflow: auto;
+            margin-bottom: 24px;
+        }
+        .bottom {
+            display: flex;
+            height: 50px;
+            margin-bottom: 24px;
+            .command-writer {
+                margin-right: 24px;
+                flex: 1;
+            }
+            .send-btn {
+                img {
+                    transform: translateX(2px);
+                }
+            }
+        }
+    }
+    .message {
+        padding: 10px 15px;
+        width: fit-content;
+        border-radius: 20px;
+        max-width: 70%;
+        font-size: 18px;
+    }
+    .message:last-of-type {
+        margin-bottom: 5px;
+    }
+    .me {
+        background-color: #2196f3;
+        color: white;
+        align-self: flex-end;
+        margin-right: 10px;
+        margin-bottom: 10px;
+    }
+    .receiver {
+        background-color: #e0e0e0;
+        align-self: auto;
+        margin-left: 10px;
+        margin-bottom: 10px;
+    }
+    .pending-info {
+        /*
+        position: absolute;
+        right: 10px;
+        bottom: 0;
+        */
+        margin-right: 10px;
+        margin-bottom: 10px;
+        align-self: flex-end;
+        font-weight: bold;
+        font-size: 12px;
+        background-color: #e0e0e0;
+        padding: 15px 15px;
+        display: flex;
+        border-radius: 40px;
+        .circle {
+            height: 10px;
+            width: 10px;
+            background-color: #424242;
+            border-radius: 50%;
+            margin: 0 5px;
+        }
+        ${createCSS()}
+    }
+
+    @keyframes animate-circle {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0.3;
+        }
+    }
+`;
+
+export default App;
