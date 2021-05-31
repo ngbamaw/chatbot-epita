2a36827b6b3febe9305dbd1b5ebfe98d92c69250
PellYm785 <williamliya@hotmail.fr>
2021-06-01 00:58:09 +0200

    feat: add PresentationMessage and MultiplePresentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import SimpleBar from 'simplebar-react';
+import SwipeableViews from 'react-swipeable-views';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import { IconButton } from '@material-ui/core';
 import SendIcon from './assets/send-icon.png';
 import PlusIcon from './assets/plus.png';
 import Style from './style';
@@ -10,6 +15,12 @@ import { Bot } from './utils/parseCommand';
 
 interface Message {
     author?: string;
+    text: string;
+}
+
+interface PresentationData {
+    title: string;
+    img: string;
     text: string;
 }
 
@@ -25,6 +36,69 @@ const MeMessage: React.FC<{ children: string }> = ({ children }) => (
     </div>
 );
 
+const PresentationMessage: React.FC<PresentationData> = ({ title, img, text }) => (
+    <div className="message presentation">
+        {/* <div className="img" style={{ backgroundImage: `url(${img})` }} /> */}
+        <img alt={title} src={img} />
+        <div className="description">
+            {title && <p className="title">{title}</p>}
+            {text && <p className="text">{text}</p>}
+        </div>
+    </div>
+);
+
+const MultiplePresantation: React.FC<{ data: PresentationData[] }> = ({ data }) => {
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step: number) => {
+        setActiveStep(step);
+    };
+    return (
+        <div className="message presentation">
+            {/* <div className="img" style={{ backgroundImage: `url(${img})` }} /> */}
+            <SwipeableViews enableMouseEvents index={activeStep} onChangeIndex={handleStepChange}>
+                {data.map(({ title, img, text }) => (
+                    <div>
+                        <img alt={title} src={img} />
+                        <div className="description">
+                            {title && <p className="title">{title}</p>}
+                            {text && <p className="text">{text}</p>}
+                        </div>
+                    </div>
+                ))}
+            </SwipeableViews>
+            <MobileStepper
+                steps={data.length}
+                position="static"
+                variant="text"
+                activeStep={activeStep}
+                nextButton={
+                    <IconButton
+                        size="small"
+                        onClick={handleNext}
+                        disabled={activeStep === data.length - 1}
+                    >
+                        <KeyboardArrowRight />
+                    </IconButton>
+                }
+                backButton={
+                    <IconButton size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        <KeyboardArrowLeft />
+                    </IconButton>
+                }
+            />
+        </div>
+    );
+};
+
 const BotElement: React.FC<{ name: string; description: string }> = ({ name, description }) => (
     <div className="bot">
         <div className="icon" />
@@ -39,10 +113,26 @@ const App: React.FC = () => {
     const [messageList, setMessageList] = React.useState<Message[]>([
         { author: 'me', text: 'test' },
         { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
+        { author: 'me', text: 'test' },
+        { author: 'receiver', text: 'Test receiver' },
     ]);
-    const [responseWriting, setResponseWriting] = React.useState<boolean>(false);
+    const [responseWriting, setResponseWriting] = React.useState(false);
     const [response, handleCommand] = useCommand(bots as Bot[]);
-    const [command, setCommand] = React.useState<string>('');
+    const [command, setCommand] = React.useState('');
     const discussionSection = React.useRef<HTMLDivElement>(null);
     const container = React.useRef<HTMLDivElement>(null);
     const scrollbarWrapper = React.useRef<HTMLDivElement>(null);
@@ -98,12 +188,45 @@ const App: React.FC = () => {
                 >
                     <div className="discussion-section" ref={discussionSection}>
                         {messageList.map(({ author, text }) => {
-                            return author === 'me' ? (
-                                <MeMessage>{text}</MeMessage>
-                            ) : (
-                                <ReceiverMessage>{text}</ReceiverMessage>
-                            );
+                            if (author === 'me') return <MeMessage>{text}</MeMessage>;
+                            return <ReceiverMessage>{text}</ReceiverMessage>;
                         })}
+                        <PresentationMessage
+                            img="https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg"
+                            title="Title"
+                            text="Description of the bot, what it can do, how it’s work, etc..."
+                        />
+                        <PresentationMessage
+                            img="https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg"
+                            title="Title"
+                            text="Description of the bot, what it can do, how it’s work, etc..."
+                        />
+                        <MultiplePresantation
+                            data={[
+                                {
+                                    title: 'Title',
+                                    text:
+                                        'Description of the bot, what it can do, how it’s work, etc...',
+                                    img:
+                                        'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg',
+                                },
+                                {
+                                    title: 'Title',
+                                    text:
+                                        'Description of the bot, what it can do, how it’s work, etc...',
+                                    img:
+                                        'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg',
+                                },
+                                {
+                                    title: 'Title',
+                                    text:
+                                        'Description of the bot, what it can do, how it’s work, etc...',
+                                    img:
+                                        'https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_960_720.jpg',
+                                },
+                            ]}
+                        />
+
                         {responseWriting && (
                             <div className="pending-info">
                                 <div className="circle" />
